07fa4390efa6e517b30b85330f9f8fac11eb6e10
Silvano Cirujano Cuesta <silvano.cirujano-cuesta@siemens.com>
2024-12-17 11:17:20 +0100

    update
    Signed-off-by: Silvano Cirujano Cuesta <silvano.cirujano-cuesta@siemens.com>

--- commit-desired-state.bash
+++ commit-desired-state.bash
@@ -6,33 +6,37 @@ THIS_SCRIPT="$(readlink -f "$0")"
 THIS_DIR="$(dirname "${THIS_SCRIPT}")"
 source "${THIS_DIR}/common.source"
 
-fail() {
-    echo "🚨 ERROR: $1"
-    exit 1
-}
-
-check_command() {
-    if command -v $1 >/dev/null ; then
-        return 0
-    else
-        fail "Command '$1' is missing"
-    fi
-}
-
 check_command regctl
 
+
+(
+    cd "${APP_NAME}"
+    regctl artifact put \
+        --artifact-type ${APP_ARTIFACT_TYPE} \
+        --file-media-type ${APP_ARCHIVE_TYPE} \
+        --file ${APP_NAME}.tgz \
+        ${REGISTRY}/${APP_NAME}:latest
+)
+
+BLOB_DIGEST=$(regctl manifest get --format=raw-body ${REGISTRY}/${APP_NAME}:latest | jq -r '.layers[] | select(.mediaType=="'${APP_ARCHIVE_TYPE}'") | .digest')
+export SCHEME
+export REGISTRY
+export APP_NAME
+export BLOB_DIGEST
+envsubst '${SCHEME},${REGISTRY},${APP_NAME},${BLOB_DIGEST}' < docker-compose-desired-state.yaml.in > docker-compose-desired-state.yaml
+
 regctl artifact put \
-    --artifact-type ${ARTIFACT_TYPE} \
+    --artifact-type ${DEPLOY_ARTIFACT_TYPE} \
     --file-media-type ${DESIRED_STATE_TYPE} \
     --file docker-compose-desired-state.yaml \
-    ${HOST}:${PORT}/${DEPLOYMENT_NAME}:desired
+    ${REGISTRY}/${DEPLOYMENT_NAME}:desired
 
 echo "Margo desired-state specification can be pulled from"
-echo "        ${HOST}:${PORT}/${DEPLOYMENT_NAME}:desired"
+echo "        ${REGISTRY}/${DEPLOYMENT_NAME}:desired"
 echo "    it can also be seen in the UI here"
-echo "        ${SCHEME}://${HOST}:${PORT}/image/${DEPLOYMENT_NAME}/tag/desired"
+echo "        ${SCHEME}://${REGISTRY}/image/${DEPLOYMENT_NAME}/tag/desired"
 
-BLOB_DIGEST=$(regctl manifest get --format=raw-body ${HOST}:${PORT}/${DEPLOYMENT_NAME}:desired | jq -r '.layers[] | select(.mediaType=="'${DESIRED_STATE_TYPE}'") | .digest')
+BLOB_DIGEST=$(regctl manifest get --format=raw-body ${REGISTRY}/${DEPLOYMENT_NAME}:desired | jq -r '.layers[] | select(.mediaType=="'${DESIRED_STATE_TYPE}'") | .digest')
 echo "Direct download (wget or get) of the desired-state YAML specification possible from"
-echo "    ${SCHEME}://${HOST}:${PORT}/v2/${DEPLOYMENT_NAME}/blobs/${BLOB_DIGEST}"
+echo "    ${SCHEME}://${REGISTRY}/v2/${DEPLOYMENT_NAME}/blobs/${BLOB_DIGEST}"
 
